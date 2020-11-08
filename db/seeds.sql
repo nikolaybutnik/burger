7f0f2ce7e1955eb71aa78b86e6e943dfd79b5487
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-11-07 20:47:23 -0500

    Created seeds.sql to populate the database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO `burgers` (`burger_name`, `devoured`)
+VALUES ("Double Whopper", false);
+INSERT INTO `burgers` (`burger_name`, `devoured`)
+VALUES ("Spicy Habanero Chicken Burger", false);
+INSERT INTO `burgers` (`burger_name`, `devoured`)
+VALUES ("Classic Angus", false);
