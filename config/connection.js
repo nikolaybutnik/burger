2415934a4d5170c8e19249a93ad3a75f70a58e15
mochi-dot-py <65631838+mochi-dot-py@users.noreply.github.com>
2020-11-12 18:36:51 -0500

    Configured connection for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
 // Load dependencies.
 const mysql = require("mysql2");
 
+let connection;
+
 // Configure mysql database connection.
-const defaultConfig = {
-  host: "localhost",
-  user: "root",
-  port: 3306,
-  password: "",
-  database: "burgers_db",
-};
-const connection = mysql.createConnection(defaultConfig).promise();
+try {
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      port: 3306,
+      password: "root",
+      database: "burgers_db",
+    });
+  }
+} catch (err) {
+  if (err) throw err;
+}
 
 // Export the connection to be used in other files.
-module.exports = connection;
+module.exports = connection.promise();
